180bb965bad390a4baf1befb76553c80e3c772e3
RayRemnant <49475467+RayRemnant@users.noreply.github.com>
2025-04-04 22:59:59 +0200

    feat: improves detection of matching existing configuration, adapted indentation of webpackConfig template literal to prettier config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.ts',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, 'api'), // Change output directory to /api
-        filename: 'main.js',
-    }
-}
+  entry: './src/main.ts',
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'api'),
+    filename: 'main.js',
+  },
+};
